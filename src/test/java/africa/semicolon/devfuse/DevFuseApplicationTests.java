c3ea9ad685b2a47857ee54b300dd7bf1ca3d70a0
Victor-Maverick <victormsonter@gmail.com>
2024-08-25 16:51:07 +0100

    adding register

---
+++ src/test/java/africa/semicolon/devfuse/DevFuseApplicationTests.java
@@ -0,0 +1,13 @@
+package africa.semicolon.devfuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevFuseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
